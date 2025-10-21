658368436129951fe7bfe1a993894424666f2bbb
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-22 00:48:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue using two Stacks/queue-using-two-stacks.java
@@ -0,0 +1,19 @@
+class StackQueue {
+    private Stack<Integer> s1;
+    private Stack<Integer> s2; 
+    public StackQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    public void push(int x) {
+        s1.push(x);
+    }
+    public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.isEmpty() ? -1 : s2.pop();
+    }
+}
