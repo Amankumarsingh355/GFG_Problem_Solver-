54c39116155cc3ae2f67e7d992efb829cf79785d
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-29 01:37:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Word Break (Trie)/word-break-trie.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int wordBreak(String A, ArrayList<String> B) {
+        if (B.contains(A))
+            return 1;
+        int n = A.length();
+        for (int i = 1; i <= n; i++) {
+            if (B.contains(A.substring(0, i)) && wordBreak(A.substring(i), B) == 1)
+                return 1;
+        }
+        return 0;
+    }
+}
