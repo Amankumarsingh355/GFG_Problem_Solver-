d0570241956c489e34ad8150b7c01b27397795d5
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-30 08:01:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Replace O's with X's/replace-os-with-xs.java
+++ Difficulty: Medium/Replace O's with X's/replace-os-with-xs.java
@@ -1,30 +1,48 @@
-class Solution{
-    static void dfs(int i, int j, char a[][], int n, int m){
-        if(i < 0 || i >= n || j < 0 || j >= m || a[i][j] != 'O') return;
-        a[i][j] = '#';
-        dfs(i-1, j, a, n, m);
-        dfs(i+1, j, a, n, m);
-        dfs(i, j-1, a, n, m);
-        dfs(i, j+1, a, n, m);
-    }
-    static char[][] fill(char a[][]) {
-        int n = a.length;
-        if (n == 0) return a;
-        int m = a[0].length;
-        for(int i = 0; i < n; i++){
-            if(a[i][0] == 'O') dfs(i, 0, a, n, m);
-            if(a[i][m-1] == 'O') dfs(i, m-1, a, n, m);
-        }
-        for(int j = 0; j < m; j++){
-            if(a[0][j] == 'O') dfs(0, j, a, n, m);
-            if(a[n-1][j] == 'O') dfs(n-1, j, a, n, m);
-        }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(a[i][j] == 'O') a[i][j] = 'X';
-                else if(a[i][j] == '#') a[i][j] = 'O';
+class Solution {
+    public void fill(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Deque<int[]> stack = new ArrayDeque<>();
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 'O') {
+                visited[i][0] = true;
+                stack.push(new int[]{i, 0});
+            }
+            if (grid[i][n - 1] == 'O') {
+                visited[i][n - 1] = true;
+                stack.push(new int[]{i, n - 1});
             }
         }
-        return a;
+        for (int j = 1; j < n - 1; j++) {
+            if (grid[0][j] == 'O') {
+                visited[0][j] = true;
+                stack.push(new int[]{0, j});
+            }
+            if (grid[m - 1][j] == 'O') {
+                visited[m - 1][j] = true;
+                stack.push(new int[]{m - 1, j});
+            }
+        }
+        while (!stack.isEmpty()) {
+            int[] cell = stack.pop();
+            int x = cell[0], y = cell[1];
+            for (int[] dir : directions) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 'O' && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    stack.push(new int[]{nx, ny});
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 'O' && !visited[i][j]) {
+                    grid[i][j] = 'X';
+                }
+            }
+        }
     }
 }
