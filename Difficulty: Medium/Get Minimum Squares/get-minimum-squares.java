8331518bfa1812416bc21f367d59876553c676da
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-11-05 03:57:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Get Minimum Squares/get-minimum-squares.java
@@ -0,0 +1,16 @@
+public class Solution {
+    private static boolean isPerfectSquare(int x) {
+        int r = (int) Math.sqrt(x);
+        return r * r == x;
+    }
+    public int minSquares(int n) {
+        if (isPerfectSquare(n)) return 1;
+        for (int i = 1; i * i <= n; i++) {
+            if (isPerfectSquare(n - i * i)) return 2;
+        }
+        int m = n;
+        while (m % 4 == 0) m /= 4;
+        if (m % 8 == 7) return 4;
+        return 3;
+    }
+}
