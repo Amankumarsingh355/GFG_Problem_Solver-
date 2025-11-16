80e749cb1f276ba9f5d8c38e57aa15c5d25715c1
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-11-17 00:08:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Sum Increasing Subsequence/max-sum-increasing-subsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSumIS(int arr[]) {
+        int n = arr.length;
+        int dp[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = arr[i];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+        int maxSum = 0;
+        for (int val : dp) {
+            maxSum = Math.max(maxSum, val);
+        }
+        return maxSum;
+    }
+}
