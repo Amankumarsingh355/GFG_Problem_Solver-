1c5079fb8dd50085db76021454b539df083c549a
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-11-24 00:59:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ 2-D Arrays | Set-1/c-2d-arrays-set1.cpp
@@ -0,0 +1,12 @@
+vector<vector<int>> transpose(int a[][M], int n)
+{
+    vector<vector<int>>t;
+    for(int i=0;i<n;i++){
+        vector<int>v;
+        for(int j=0;j<n;j++)
+            v.push_back(a[j][i]);
+        t.push_back(v);
+        
+    }
+    return t;
+}
