1d161fb0c2980933b0be749e934be9aafcf7dabf
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-06 01:34:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest product/largest-product.java
@@ -0,0 +1,17 @@
+/*You are required to complete the function*/
+class Solution {
+    public int findMaxProduct(int[] arr, int k) {
+        int  max = 0;
+        for(int i=0; i<arr.length; i++){
+            int j=i, multi = 1, c = 0;
+            while(j<arr.length&&c<k){
+                multi*=arr[j++];
+                c++;
+            }
+            if(c==k){
+                max = Math.max(max, multi);
+            }
+        }
+        return max;
+    }
+}
