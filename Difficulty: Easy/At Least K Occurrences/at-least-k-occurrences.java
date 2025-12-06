2c5c7a7c65bb6edf2c4a31d6f7c8a93aef0eeb95
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-07 02:40:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/At Least K Occurrences/at-least-k-occurrences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int firstElementKTime(int[] arr, int k) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int x : arr)
+        {
+            hm.put(x,hm.getOrDefault(x,0)+1);
+            if(hm.get(x)==k)
+             return x;
+        }
+        return -1;
+    }
+}
