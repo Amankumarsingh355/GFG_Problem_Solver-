f2fd5f0b4a967d2c0eb91df16a0d6131673f3ece
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-09 01:40:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.java
@@ -0,0 +1,13 @@
+class Solution {
+    String removeDuplicates(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        String ans = "";
+        for(int i=0;i<s.length();i++) {
+            if(!map.containsKey(s.charAt(i))) {
+                ans+=s.charAt(i);
+            }
+            map.put(s.charAt(i), 1);
+        }
+        return ans;
+    }
+}
