636a75334a6acaa5151c254a4d76464a9e2f46f8
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-13 04:18:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with difference k/pairs-with-difference-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    int countPairs(int[] arr, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int val:arr){
+            map.put(val,map.getOrDefault(val,0)+1);
+        }
+        int sum=0;
+        for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+            int cur=entry.getKey();
+            int val=cur+k;
+            if (map.containsKey(val)){
+                sum+=(map.get(val)*map.get(cur));
+            }
+        }
+        return sum;
+    }
+}
+
