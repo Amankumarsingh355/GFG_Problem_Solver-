7c478c6350a3136a0b8563fb800b956a48bf6ba4
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-14 01:56:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a row-wise sorted matrix/search-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchRowMatrix(int[][] mat, int x) {
+        int i,j;
+        for(i=0;i<mat.length;i++)
+        {
+            for(j=0;j<mat[i].length;j++)
+            {
+                if(mat[i][j]==x)return true;
+            }
+        }
+        return false;
+    }
+}
