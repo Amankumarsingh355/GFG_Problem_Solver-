73495fe801f6b5086f148dd281b783dc1e520f46
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-20 03:17:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Run Length Encoding/run-length-encoding.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static String encode(String s) {
+        StringBuilder sb = new StringBuilder();
+        int c = 1;
+        for(int i = 1; i <= s.length(); i++){
+            if(i < s.length() && s.charAt(i) == s.charAt(i - 1)){
+                c++;
+            }
+            else{
+                sb.append(s.charAt(i - 1)).append(c);
+                c = 1;
+            }
+        }
+        return sb.toString();
+    }
+}
