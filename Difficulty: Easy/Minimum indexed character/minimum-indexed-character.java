8094895e1f02e93a1f4e05b2c5038d47aa13b0a7
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-20 03:13:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum indexed character/minimum-indexed-character.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int minIndexChar(String s1, String s2) {
+        Set<Character> set = new HashSet<>();
+        for (char ch : s2.toCharArray()) {
+            set.add(ch);
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (set.contains(s1.charAt(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}  
