6e626f40cc64c037751f930621f1aa42e31620ae
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-22 13:10:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Wifi Range/wifi-range.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean wifiRange(int N, String S, int X) {
+       boolean []seen = new boolean[N];
+       Arrays.fill(seen,false);
+       for(int i=0;i<N;i++)
+       {
+           if(S.charAt(i) == '1')
+           {
+               for(int j = Math.max(0 , i - X); j <= Math.min(i + X , N - 1) ; j++)
+               {
+                   seen[j] = true;
+               }
+           }
+       }
+       for(int i=0;i<N;i++)
+       {
+           if(!seen[i])
+           {
+               return false;
+           }
+       }
+       return true;
+    }
+}
