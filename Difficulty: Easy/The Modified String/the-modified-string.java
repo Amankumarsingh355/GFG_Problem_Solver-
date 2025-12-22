611325a94942a63ae9073f8c1ea3056c0664e6e8
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-22 13:11:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The Modified String/the-modified-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static long modified(String a)
+    {
+        long len = 1;
+        long cnt = 0;
+        for(int i=1; i<a.length(); i++){
+            if(a.charAt(i) == a.charAt(i-1)){
+                len++;
+                if(len==3){
+                    cnt++;
+                    len -= 2;
+                }
+            }else len = 1;
+        }
+        return cnt;
+    }
+}
