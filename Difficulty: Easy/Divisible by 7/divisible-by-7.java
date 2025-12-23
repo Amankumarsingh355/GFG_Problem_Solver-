b5568215f54fab8fcc7c7cfda07eecc99b297059
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-23 19:41:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Divisible by 7/divisible-by-7.java
@@ -0,0 +1,11 @@
+class Solution {
+    int isdivisible7(String num) {
+        int n = num.length();
+        int check =0;
+        for(int i=0; i<n; i++){
+            check = (check*10) +(num.charAt(i)-'0');
+            check %= 7;
+        }
+        return (check==0) ?1:0;
+    }
+}
