d4e7859d8dc3139891a98d134e77bd001afb1cb8
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-24 20:15:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remainder with 7/remainder-with-7.java
@@ -0,0 +1,10 @@
+class Solution {
+    int remainderWith7(String num) {
+        int rem = 0;
+        for (int i = 0; i < num.length(); i++) {
+            int digit = num.charAt(i) - '0';
+            rem = (rem * 10 + digit) % 7;    
+        }
+        return rem;
+    }
+}
