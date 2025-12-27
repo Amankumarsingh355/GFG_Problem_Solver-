c75f3f08c56185187720f4e638a83eff013dc760
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-27 18:17:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Distinct Substrings/distinct-substrings.java
@@ -0,0 +1,12 @@
+class Solution 
+{ 
+    int fun(String s) 
+    {
+       HashSet<String> hs= new HashSet<>();
+         String sb="";
+       for(int i=0;i<s.length()-1;i++){
+         sb=s.substring(i,i+2);
+         hs.add(sb);}
+      return hs.size();
+    }
+} 
