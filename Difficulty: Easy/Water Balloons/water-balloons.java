52358eaff462dd19cb271efc6237e94dc9914918
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-29 00:00:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Water Balloons/water-balloons.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+
+class Solution {
+    static String sequence(String st){
+        // code here{
+        int count = 0;
+        char element  = st.charAt(0);
+        StringBuilder result = new StringBuilder();
+        for(int i=0;i<st.length();i++) {
+            char ch = st.charAt(i);
+            if(ch ==element && count <2) {
+                result.append(String.valueOf(ch));
+                count++;
+            }
+            else if(ch!=element && count<=2) {
+                result.append(String.valueOf(ch));
+                element =ch;
+                count = 1;
+            }
+        }
+        return result.toString();
+    }
+}
