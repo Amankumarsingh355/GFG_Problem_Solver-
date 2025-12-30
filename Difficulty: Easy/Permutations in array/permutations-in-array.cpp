2a2e95acb993d2b366d9fa685f0b9abbc61c8f05
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-30 22:50:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Permutations in array/permutations-in-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
+        sort(arr1.begin(), arr1.end());
+        sort(arr2.begin(), arr2.end());
+        for(int i = 0;i<arr1.size();i++){
+            if(arr1[i]+arr2[arr2.size()-1-i]<k) return 0;
+        }
+        return 1 ;
+    }
+};
