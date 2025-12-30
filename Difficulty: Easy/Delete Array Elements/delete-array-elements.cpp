9ac315cce1107ceb5a68c7d278b5cde7fc7b3166
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-30 23:22:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Array Elements/delete-array-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<int> deleteElement(vector<int>& arr, int k) {
+        stack<int> st;
+        vector<int> ans;
+        int count=0;
+        for(int i=0;i<=arr.size()-1;i++){
+            while(!st.empty() && st.top()<arr[i] && count<k){
+                st.pop();
+                count++;
+            }
+            
+            st.push(arr[i]);
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
