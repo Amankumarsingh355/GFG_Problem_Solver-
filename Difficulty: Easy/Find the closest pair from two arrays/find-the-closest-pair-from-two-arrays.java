385e21a0068e5a0e7b3b05fa2852d0be22345d04
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-02-28 23:50:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the closest pair from two arrays/find-the-closest-pair-from-two-arrays.java
+++ Difficulty: Easy/Find the closest pair from two arrays/find-the-closest-pair-from-two-arrays.java
@@ -1,28 +1,28 @@
-class Solution{
-    public static ArrayList<Integer> printClosest (int arr[], int brr[], int n, int m, int x) {
-        int dim = 0;int dip = brr.length - 1;
-        int result[] = new int[2];
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int analyser = Integer.MAX_VALUE;
-        while(dim < arr.length && dip >= 0 )
-        {
-            int sum = arr[dim] + brr[dip];
-            if(Math.abs(sum - x) < analyser )
-            {
-                analyser = Math.abs(sum - x);
-                result[0] = arr[dim];
-                result[1] = brr[dip];
-            }
-            if(sum <= x)
-            {
-                dim++;
-            }
-            else
-            {
-                dip--;
-            }
+class Solution {
+    public static ArrayList<Integer> findClosestPair(int arr1[], int arr2[], int x) {
+        int ans1=0;
+        int ans2=0;
+        int diff=Integer.MAX_VALUE;
+        int i=0;
+        int j=arr2.length-1;
+        while(i<arr1.length && j>=0){
+               int d=Math.abs((arr1[i]+arr2[j])-x);
+               if(d<diff){
+                   diff=d;
+                   ans1=arr1[i];
+                   ans2=arr2[j];
+               }
+               if(arr1[i]+arr2[j]>x){
+                   j--;
+               }
+               else if(arr1[i]+arr2[j]<=x){
+                   i++;
+               }
         }
-        list.add(result[0]);list.add(result[1]);
-        return list;
+        ArrayList<Integer>ans=new ArrayList<>();
+        ans.add(ans1);
+        ans.add(ans2);
+        return ans;
     }
 }
+
