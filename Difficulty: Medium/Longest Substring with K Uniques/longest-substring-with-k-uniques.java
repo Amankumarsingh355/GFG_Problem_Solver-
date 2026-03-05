0b1dbccfd12ba59befa1871860033f4e2a27a6bf
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-03-05 19:07:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -1,29 +1,27 @@
-public class Solution {
+class Solution {
     public int longestKSubstr(String s, int k) {
-        if (s == null || s.length() == 0 || k <= 0) return -1;
-        int n = s.length();
-        int[] freq = new int[26];
-        int distinct = 0;
-        int left = 0;
-        int maxLen = -1;
-        for (int right = 0; right < n; right++) {
-            int r = s.charAt(right) - 'a';
-            if (freq[r]++ == 0) distinct++;
-            while (distinct > k && left <= right) {
-                int l = s.charAt(left) - 'a';
-                if (--freq[l] == 0) distinct--;
-                left++;
+        // code here
+        HashMap<Character,Integer>map = new HashMap<>();
+        int ans=-1;
+        int j=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+            while(map.size()>k)
+            {
+                map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)-1);
+                if(map.get(s.charAt(j))==0)
+                map.remove(s.charAt(j));
+                j++;
             }
-            if (distinct == k) maxLen = Math.max(maxLen, right - left + 1);
+            if(map.size()==k)
+            {
+                ans=Math.max(ans,i-j+1);
+            }
         }
-        return maxLen;
-    }
-    public static void main(String[] args) {
-        Solution obj = new Solution();
-        String s = "aabacbebebe";
-        int k = 3;
-        System.out.println(obj.longestKSubstr(s, k));
-        System.out.println(obj.longestKSubstr("aaaa", 2));
-        System.out.println(obj.longestKSubstr("aabaaab", 2));
+        
+        return ans;
     }
 }
+
