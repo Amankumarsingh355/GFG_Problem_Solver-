99d5e54d88f9a4f593dcb7db3af12e8be1e010cf
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-03-14 11:43:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -0,0 +1,43 @@
+class Solution {
+    class CustomNode{
+        Node node;
+        int col;
+        CustomNode(Node node , int col){
+            this.node = node;
+            this.col = col;
+        }
+    }
+    public ArrayList<Integer> topView(Node root) {
+        // code here
+        ArrayList<Integer> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        int minCol = Integer.MAX_VALUE;
+        int maxCol = Integer.MIN_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Queue<CustomNode> q = new LinkedList<>();
+        q.offer(new CustomNode(root , 0));
+        while(!q.isEmpty()){
+            CustomNode currNode = q.poll();
+            Node node = currNode.node;
+            int col = currNode.col;
+            
+            if(!map.containsKey(col)){
+                map.put(col , node.data);
+                minCol = Math.min(minCol , col);
+                maxCol = Math.max(maxCol , col);
+            }
+            if(node.left != null){
+                q.offer(new CustomNode(node.left , col - 1));
+            }
+            if(node.right != null){
+                q.offer(new CustomNode(node.right , col + 1));
+            }
+        }
+        for(int i = minCol ; i <= maxCol ; i++){
+            result.add(map.get(i));
+        }
+        return result;
+    }
+}
