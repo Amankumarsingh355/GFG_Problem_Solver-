971ee3688c76cfb169ee92fd554d92b1d86fa749
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-03-21 13:30:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
+++ Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
@@ -1,37 +1,38 @@
-public class Solution {
-    private int[] catalanNumbers(int n) {
-        int[] catalan = new int[n + 1];
-        catalan[0] = 1;
-        catalan[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            catalan[i] = 0;
-            for (int j = 0; j < i; j++) {
-                catalan[i] += catalan[j] * catalan[i - j - 1];
-            }
-        }
-        return catalan;
-    }
+class Solution {
     public ArrayList<Integer> countBSTs(int[] arr) {
-        int n = arr.length;
-        int[] catalan = catalanNumbers(n);
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int L = 0, R = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] < arr[i]) L++;
-                else if (arr[j] > arr[i]) R++;
-            }
-            result.add(catalan[L] * catalan[R]);
-        }
-        return result;
+        int n=arr.length;
+    long[]cat=getCatlan(n);
+    int[]sortArray=arr.clone();
+    Arrays.sort(sortArray);
+
+    ArrayList<Integer>ans=new ArrayList<>();
+    for(int e:arr){
+        int leftCnt=findFirstOcc(sortArray,e);
+        int rightCnt=n-1-leftCnt;
+        ans.add((int)(cat[leftCnt]*cat[rightCnt]));
     }
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] arr = {2, 1, 3};
-        ArrayList<Integer> result = sol.countBSTs(arr);
-        for (int x : result) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
+     return ans;  
     }
+    private long[]getCatlan(int n){
+        long[]c=new long[n+1];
+        c[0]=1;
+        if(n>0) c[1]=1;
+        for(int i=2;i<=n;i++)
+        for(int j=0;j<i;j++){
+            c[i]+=(c[j]*c[i-1-j]);
+        }
+        return c;
+    }
+    private int findFirstOcc(int[]arr,int target){
+        int low=0,high=arr.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<target)
+                low=mid+1;
+            else
+            high=mid;
+        }
+        return low;
+        }
+
 }
