54f5890974266c2e06b9b1d843dd035620ff83fa
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-05-03 08:47:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort by Set Bit Count/sort-by-set-bit-count.java
+++ Difficulty: Easy/Sort by Set Bit Count/sort-by-set-bit-count.java
@@ -1,30 +1,11 @@
-class Compute  
-{ 
-    static void sortBySetBitCount(Integer arr[], int n)
-    { 
-        int[][] arr_list = new int[21][100000];
-        int[] index = new int[21];
-        for(int x : arr){
-            int num = x;
-            int zeros = 0;
-            while(x > 0){
-                zeros += x%2;
-                x = x/2;
-            }
-            arr_list[zeros][index[zeros]++] = num;
-        }
-        int counter = 0;
-        for(int i=20; i>0; i--){
-            for(int j=0; j<100000; j++){
-                if(arr_list[i][j] != 0){
-                    arr[counter++] = arr_list[i][j];
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        
-    } 
+class Solution {
+    ArrayList<Integer> sortBySetBitCount(int[] arr) {
+        Integer[] boxed = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(boxed, (a, b) -> {
+            int countA = Integer.bitCount(a);
+            int countB = Integer.bitCount(b);
+            return Integer.compare(countB, countA); // descending order
+        });
+        return new ArrayList<>(Arrays.asList(boxed));
+    }
 }
- 
