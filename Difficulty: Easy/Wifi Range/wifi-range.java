9aac0dd2d6e536fd305be2765644f02b9c45dafd
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-05-27 13:11:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Wifi Range/wifi-range.java
+++ Difficulty: Easy/Wifi Range/wifi-range.java
@@ -1,24 +1,27 @@
 class Solution {
-    boolean wifiRange(int N, String S, int X) {
-       boolean []seen = new boolean[N];
-       Arrays.fill(seen,false);
-       for(int i=0;i<N;i++)
-       {
-           if(S.charAt(i) == '1')
-           {
-               for(int j = Math.max(0 , i - X); j <= Math.min(i + X , N - 1) ; j++)
-               {
-                   seen[j] = true;
-               }
-           }
-       }
-       for(int i=0;i<N;i++)
-       {
-           if(!seen[i])
-           {
-               return false;
-           }
-       }
-       return true;
+    public boolean wifiRange(String s, int x) {
+        int l = Integer.MAX_VALUE;
+        int r = Integer.MIN_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '1') {
+                int lx = i - x;
+                if (lx < 0) lx = 0;
+                int ry = i + x;
+                if (ry >= s.length()) ry = s.length()-1;
+                if (lx == 0 && ry == s.length()-1) return true;
+                if (l == Integer.MAX_VALUE && r == Integer.MIN_VALUE) {
+                    l = lx;
+                    r = ry;
+                } else {
+                    if (lx > r && lx - r > 1) {
+                        return false;
+                    }
+                    l = Math.min(l, lx);
+                    r = Math.max(r, ry);
+                }
+            }
+        }
+        return l == 0 && r == s.length()-1;
     }
 }
