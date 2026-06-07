26336d0a6b809a7351737836e2ceffb8fbe978e6
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-06-07 14:33:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Finding Profession/finding-profession.java
+++ Difficulty: Medium/Finding Profession/finding-profession.java
@@ -1,13 +1,6 @@
-public class Solution {
-    public char profession(long level, long pos) {
-        long flips = Long.bitCount(pos - 1);
-        return (flips % 2 == 0) ? 'e' : 'd';
-    }
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.profession(4, 2));
-        System.out.println(sol.profession(3, 4));
-        System.out.println(sol.profession(1, 1));
-        System.out.println(sol.profession(5, 11));
+class Solution {
+    public String profession(int level, int pos) {
+        int flips = Integer.bitCount(pos - 1); 
+        return (flips % 2 == 0) ? "Engineer" : "Doctor";
     }
 }
