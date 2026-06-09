ec889c67618c0d42647d1652f120a752af08c89a
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-06-09 17:10:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Seating Arrangement/seating-arrangement.java
+++ Difficulty: Easy/Seating Arrangement/seating-arrangement.java
@@ -1,43 +1,16 @@
 class Solution {
-    public static boolean is_possible_to_get_seats(int n, int m, int[] seats) {
-        if(n == 0){
-            return true;
-        }
-        if(m < n){
-            return false;
-        }
-        if(m == 1){
-            if(seats[0] == 0){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        if(seats[0] == 0 && seats[1] == 0){
-            n--;
-            seats[0] = 1;
-            if(n == 0){
-                return true;
-            }
-        }
-        if(seats[m-1] == 0 && seats[m-2] == 0){
-            n--;
-            seats[m-1] = 1;
-            if(n == 0){
-                return true;
-            }
-        }
-        for(int i=1; i<m-1; i++){
-            if(seats[i-1] == 0 && seats[i] == 0 && seats[i+1] == 0){
-                n--;
-                seats[i] = 1;
-                i++;
-            }
-            if(n == 0){
-                return true;
-            }
-        }
-        return false;
+    public boolean canSeatAllPeople(int k, int[] seats) {
+         int n=seats.length;
+         for(int i=0;i<n-1;i++){
+             if(seats[i]==1 && seats[i+1]==1) return false;
+         }
+         for(int i=0;i<n;i++){
+             if((i==0 || seats[i-1]!=1) && (i==n-1 || seats[i+1]!=1) && seats[i]==0 && k>0){
+                  k--;
+                  seats[i]=1;
+             }
+         }
+         if(k==0) return true;
+         return false;
     }
 }
