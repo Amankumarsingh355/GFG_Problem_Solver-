22ca85ba80579e5f34ae264237d1e66231f99021
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-07-13 23:23:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Rearrange the Array/rearrange-the-array.java
+++ Difficulty: Hard/Rearrange the Array/rearrange-the-array.java
@@ -10,13 +10,11 @@ class Solution {
         return true;
     }
     int minOperations(int[] b) {
-        // code here
         int len=b.length;
         int count=0;
         int arr[]=new int[len];
         for(int i=0;i<len;i++)
         arr[i]=i+1;
-        
         do
         {
             int next[]=new int[len];
